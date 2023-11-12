b6c41050fa17bf7afbbf725f5354a01381541f4e
hasssnnn <125768897+hasssnnn@users.noreply.github.com>
2023-11-12 22:41:04 +0200

    add splash view

--- lib/Features/Splash/Presentation/Views/Widgets/splash_view_body.dart
+++ lib/Features/Splash/Presentation/Views/Widgets/splash_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:book_nook/Core/utils/assets_data.dart';
 import 'package:flutter/material.dart';
 
 class SplashViewBody extends StatelessWidget {
@@ -5,6 +6,21 @@ class SplashViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch, // take the biggest width size
+        children: [
+          Image.asset(AssetsData.logo,scale: 2.5,),
+          const Center(
+            child:  Text('Limitless Books', style: TextStyle(
+              fontSize: 22,
+              fontWeight: FontWeight.w500,
+              color: Colors.white
+            ),),
+          )
+        ],
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-import 'Features/Splash/Presentation/Views/splash_view.dart';
+import 'Features/splash/presentation/views/splash_view.dart';
+import 'consts.dart';
 
 void main() {
   runApp(const BookNook());
@@ -14,7 +15,11 @@ class BookNook extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      home: SplashView(),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData().copyWith(
+        scaffoldBackgroundColor:  kPrimaryColor
+      ),
+      home: const SplashView(),
     );
   }
 }
